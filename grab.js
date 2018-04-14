39a72380fca2a1ab94325468513175f580031152
Squishy123 <christian.gnaw@gmail.com>
2018-04-14 18:08:56 -0400

    Added parallel scraping

--- grab.js
+++ grab.js
@@ -67,19 +67,80 @@ async function getRapidVideoFile(page, url) {
         await page.waitForSelector('#videojs > div.vjs-error-display.vjs-modal-dialog > div', { timeout: 5000 });
     } catch (err) { }
 
-    return [...new Set(links)];
+    return [...new Set(links)][0];
+}
+
+/**
+ * Grabs the links of all episodes on the given source page
+ * @param {String} url 
+ */
+async function getSourceLinks(page, url) {
+    await page.goto(url);
+    try {
+        await page.waitForSelector('#main > div > div.widget.servers', { timeout: 5000 });
+    } catch (err) { }
+
+    let range = await page.evaluate(() => {
+        return (document.querySelector('#main > div > div.widget.servers > div.widget-body > div:nth-child(1) > div')) ? document.querySelector('#main > div > div.widget.servers > div.widget-body > div:nth-child(1) > div').children.length : 1;
+    });
+
+    let sources = await page.evaluate((range) => {
+        let sources = [];
+        let servers = document.querySelector('#main > div > div.widget.servers > div.widget-body').children;
+        for (let p = 0; p < servers.length; p++) {
+            sources.push({ dataName: servers[p].getAttribute('data-name'), sourceList: [] });
+            for (let r = 0; r < range; r++) {
+                let list = document.querySelector(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p + 1}) > ul:nth-child(${r + 2})`).children;
+                for (let l = 0; l < list.length; l++) {
+                    sources[p].sourceList.push(list[l].children[0].href);
+                }
+            }
+        }
+        return sources;
+    }, range);
+
+    return sources;
 }
 
 (async () => {
+    let start = new Date();
     let browser = await puppeteer.launch({
-        headless: false,
-        args: ["--disable-web-security"]
+        headless: false
+        //args: ["--disable-web-security"]
         //executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
     });
     let page = await browser.newPage();
-    let player = await getRapidVideoPlayer(page, 'https://www4.9anime.is/watch/sword-art-online-alternative-gun-gale-online.m7j7/q64v1v');
-    let file = await getRapidVideoFile(page, `${player}&q=1080p`);
-    console.log(file);
+    let sources = await getSourceLinks(page, 'https://www4.9anime.is/watch/dragon-ball-super.7jly/k4j9nr');
+    jsonfile.writeFileSync('sources.json', sources);
+
     page.close();
+
+    let chunks = ((arr, chunkSize) => {
+        let results = [];
+        while (arr.length) {
+            results.push(arr.splice(0, chunkSize))
+        }
+        return results;
+    })(sources[0].sourceList, 5);
+
+    async function loadChunk(chunk) {
+        let data = [];
+        for (let i = 0; i < chunk.length; i++) {
+                let p = await browser.newPage();
+                let player = await getRapidVideoPlayer(p, chunk[i]);
+                let file = await getRapidVideoFile(p, `${player}&q=1080p`);
+                console.log(`${file}`);
+                data[i] = file;
+                p.close();
+        }
+        return data;
+    }
+    let files = await Promise.all([loadChunk(chunks[0]), loadChunk(chunks[1]), loadChunk(chunks[2]),  loadChunk(chunks[3]),  loadChunk(chunks[4])]);
+    jsonfile.writeFileSync('files.json', [].concat(...files));
+
+    //sources[0].forEach
+
+    // console.log(file);
     browser.close();
+    console.log(`Execution Time: ${new Date() - start}`);
 })();
