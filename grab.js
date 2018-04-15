0fd06da319f832e3a1cd7487cd9ac48849a5ead2
Squishy123 <christian.gnaw@gmail.com>
2018-04-14 23:30:19 -0400

    Added single range case

--- grab.js
+++ grab.js
@@ -12,7 +12,7 @@ const whitelist = ["aspx",
     "9anime",
     "disqus"];
 
-const blacklist = ['mc.yandex.ru']
+const blacklist = ['mc.yandex.ru', 'bebi']
 
 /**
 * Gets the rapidvideo player given a 9anime link
@@ -77,20 +77,24 @@ async function getRapidVideoFile(page, url) {
 async function getSourceLinks(page, url) {
     await page.goto(url);
     try {
-        await page.waitForSelector('#main > div > div.widget.servers', { timeout: 5000 });
+        await page.waitForSelector('#main > div > div.widget.servers > div.widget-body', { timeout: 5000 });
     } catch (err) { }
 
     let range = await page.evaluate(() => {
         return (document.querySelector('#main > div > div.widget.servers > div.widget-body > div:nth-child(1) > div')) ? document.querySelector('#main > div > div.widget.servers > div.widget-body > div:nth-child(1) > div').children.length : 1;
     });
-
     let sources = await page.evaluate((range) => {
         let sources = [];
         let servers = document.querySelector('#main > div > div.widget.servers > div.widget-body').children;
         for (let p = 0; p < servers.length; p++) {
             sources.push({ dataName: servers[p].getAttribute('data-name'), sourceList: [] });
+            let list;
             for (let r = 0; r < range; r++) {
-                let list = document.querySelector(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p + 1}) > ul:nth-child(${r + 2})`).children;
+                if (range == 1) {
+                    list = document.querySelector(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p + 1}) > ul`).children;
+                } else {
+                    list = document.querySelector(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p + 1}) > ul:nth-child(${r + 2})`).children;
+                }
                 for (let l = 0; l < list.length; l++) {
                     sources[p].sourceList.push(list[l].children[0].href);
                 }
@@ -102,15 +106,28 @@ async function getSourceLinks(page, url) {
     return sources;
 }
 
+async function loadChunk(browser, chunk) {
+    let data = []
+    for (let i = 0; i < chunk.length; i++) {
+        let p = await browser.newPage();
+        let player = await getRapidVideoPlayer(p, chunk[i]);
+        let file = await getRapidVideoFile(p, `${player}&q=1080p`);
+        data.push(file);
+        //console.log(`${file}`);
+        await p.close();
+    }
+    return data;
+}
+
 (async () => {
     let start = new Date();
     let browser = await puppeteer.launch({
-        headless: false
+        headless: true
         //args: ["--disable-web-security"]
         //executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
     });
     let page = await browser.newPage();
-    let sources = await getSourceLinks(page, 'https://www4.9anime.is/watch/dragon-ball-super.7jly/k4j9nr');
+    let sources = await getSourceLinks(page, 'https://www4.9anime.is/watch/tokyo-ghoulre.2yx0/035qr5');
     jsonfile.writeFileSync('sources.json', sources);
 
     page.close();
@@ -121,23 +138,19 @@ async function getSourceLinks(page, url) {
             results.push(arr.splice(0, chunkSize))
         }
         return results;
-    })(sources[0].sourceList, 5);
-
-    async function loadChunk(chunk) {
-        let data = [];
-        for (let i = 0; i < chunk.length; i++) {
-                let p = await browser.newPage();
-                let player = await getRapidVideoPlayer(p, chunk[i]);
-                let file = await getRapidVideoFile(p, `${player}&q=1080p`);
-                console.log(`${file}`);
-                data[i] = file;
-                p.close();
-        }
-        return data;
-    }
-    let files = await Promise.all([loadChunk(chunks[0]), loadChunk(chunks[1]), loadChunk(chunks[2]),  loadChunk(chunks[3]),  loadChunk(chunks[4])]);
+    })(sources[0].sourceList, 24);
+    /** 
+    chunks.forEach(c => {
+        console.log(c)
+    });*/
+    let promises = [];
+    chunks.forEach((e) => {
+        promises.push(loadChunk(browser, e));
+    });
+    let files = await Promise.all(promises);
+    //let files = await loadChunk(sources[0].sourceList)
     jsonfile.writeFileSync('files.json', [].concat(...files));
-
+    // jsonfile.writeFileSync('files.json', files);
     //sources[0].forEach
 
     // console.log(file);
