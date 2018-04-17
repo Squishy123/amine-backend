9dcdc44575dfc3d0b667517eb2607c29ea1f757a
Christian Wang <christian.gnaw@gmail.com>
2018-04-16 21:54:55 -0400

    Reorganized dir structure

---
+++ src/lib/9anime.js
@@ -0,0 +1,153 @@
+const puppeteer = require('puppeteer');
+const jsonfile = require('jsonfile');
+
+const properties = require('./properties.js');
+const whitelist = properties.whitelist;
+const blacklist = properties.blacklist;
+
+
+/**
+* Gets the rapidvideo player given a 9anime link
+ */
+async function getRapidVideoPlayer(page, url) {
+
+    //block popups
+    await page.evaluateOnNewDocument(() => {
+        window.open = () => null;
+    });
+
+    await page.goto(url);
+    try {
+        await page.waitForSelector('#player', { visible: true });
+    } catch (err) { }
+    await page.click('#player');
+
+
+    try {
+        await page.waitForSelector('#player > iframe');
+    } catch (err) { }
+
+    let link = page.evaluate(() => {
+        return document.querySelector('#player > iframe').src;
+    })
+    return link;
+}
+
+/**
+* Gets the rapidvideo player given a rapidvideo player url
+ */
+async function getRapidVideoFile(page, url) {
+    let links = [];
+    await page.setRequestInterception(true);
+    page.on('request', interceptedRequest => {
+        let url = interceptedRequest.url().toString();
+        let wl = whitelist.some((e) => {
+            return url.includes(e);
+        });
+        let bl = blacklist.some((e) => {
+            return url.includes(e);
+        })
+        if (wl && !bl) {
+            if (url.includes("mp4")) links.push(url);
+            interceptedRequest.continue();
+        }
+        else interceptedRequest.abort();
+    });
+
+    await page.goto(url);
+    try {
+        await page.waitForSelector('#videojs > div.vjs-error-display.vjs-modal-dialog > div', { timeout: 5000 });
+    } catch (err) { }
+
+    return [...new Set(links)][0];
+}
+
+/**
+ * Grabs the links of all episodes on the given source page
+ * @param {String} url 
+ */
+async function getSourceLinks(page, url) {
+    await page.goto(url);
+    try {
+        await page.waitForSelector('#main > div > div.widget.servers > div.widget-body', { timeout: 5000 });
+    } catch (err) { }
+
+    let range = await page.evaluate(() => {
+        return (document.querySelector('#main > div > div.widget.servers > div.widget-body > div:nth-child(1) > div')) ? document.querySelector('#main > div > div.widget.servers > div.widget-body > div:nth-child(1) > div').children.length : 1;
+    });
+    let sources = await page.evaluate((range) => {
+        let sources = [];
+        let servers = document.querySelector('#main > div > div.widget.servers > div.widget-body').children;
+        for (let p = 0; p < servers.length; p++) {
+            sources.push({ dataName: servers[p].getAttribute('data-name'), sourceList: [] });
+            let list;
+            for (let r = 0; r < range; r++) {
+                if (range == 1) {
+                    list = document.querySelector(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p + 1}) > ul`).children;
+                } else {
+                    list = document.querySelector(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p + 1}) > ul:nth-child(${r + 2})`).children;
+                }
+                for (let l = 0; l < list.length; l++) {
+                    sources[p].sourceList.push(list[l].children[0].href);
+                }
+            }
+        }
+        return sources;
+    }, range);
+
+    return sources;
+}
+async function grabLink(browser, url) {
+    let p = await browser.newPage();
+    let player = await getRapidVideoPlayer(p, url);
+    let file = await getRapidVideoFile(p, `${player}`);
+    await p.close();
+    return file
+}
+
+async function loadChunk(browser, chunk, num) {
+    let data = []
+    for (let i = 0; i < chunk.length; i++) {
+        let file = await grabLink(browser, chunk[i]);
+        if (file) {
+            data.push(file);
+            console.log(`${i / chunk.length * 100}% of chunk ${num} completed`)
+            jsonfile.writeFileSync('../../tmp/raw.json', file, { flag: 'a' });
+        } else {
+            console.log(`Error limit passed: Chunk ${num} error at : ${chunk[i]}`)
+            jsonfile.writeFileSync('../../tmp/errlog.json', chunk[i], { flag: 'a' });
+            return;
+        }
+    }
+    console.log(`Chunk ${num} completed`)
+    return data;
+}
+
+(async () => {
+    let start = new Date();
+    let browser = await puppeteer.launch();
+    let page = await browser.newPage();
+
+
+    let sources = await getSourceLinks(page, 'https://www4.9anime.is/watch/one-punch-man.928/q2w2rw')
+    console.log("Completed Source Link Scrape")
+    page.close();
+
+    let chunks = ((arr, chunkSize) => {
+        let results = [];
+        while (arr.length) {
+            results.push(arr.splice(0, chunkSize))
+        }
+        return results;
+    })(sources[0].sourceList, Math.ceil(sources[0].sourceList.length / 10));
+    let promises = [];
+    chunks.forEach((e, i) => {
+        console.log(`Loaded Chunk ${i + 1} of ${chunks.length}`)
+        promises.push(loadChunk(browser, e, i + 1));
+    });
+    let files = await Promise.all(promises);
+    console.log(files);
+    jsonfile.writeFileSync('../../tmp/data.json', [].concat(...files), {flag: 'wx'});
+    browser.close();
+    console.log(`Execution Time: ${new Date() - start}`);
+})();
---
+++ src/lib/properties.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "blacklist": [
+        "mc.yandex.ru",
+        "bebi"
+    ],
+    "whitelist": [
+        "aspx",
+        "axd",
+        "html",
+        "js",
+        "css",
+        "rapidvideo",
+        "mp4",
+        "video",
+        "9anime",
+        "disqus"
+    ]
+}
