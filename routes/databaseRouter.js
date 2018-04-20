1630a75352a88e0525c2a2b074cacfbcc2d3b5bc
Christian Wang <christian.gnaw@gmail.com>
2018-04-20 13:28:12 -0500

    Added anime lookup route

--- routes/databaseRouter.js
+++ routes/databaseRouter.js
@@ -9,19 +9,43 @@ router.get('/', (req, res) => {
 
 function lookupAnime(req, res, next) {
     let title = req.params.animetitle;
-    mongoose.connect("mongodb://localhost:27017/media");   
-    Anime.find({title: title}, (err, animes) => {
-        if(err) {
-            return res.status('500').send({errors: err});
-        }
+    (async () => {
+        await mongoose.connect("mongodb://localhost:27017/media");
+        await Anime.find({ title: title }, (err, animes) => {
+            if (err) {
+                return res.status('500').send({ errors: err });
+            }
 
-        if(animes.length == 0) return res.status(404).send({errors: 'No Animes Found'})
-        req.results = animes;
-        next();
-    })
+            if (animes.length == 0) return res.status(404).send({ errors: 'No Animes Found' })
+            req.results = animes;
+            next();
+        });
+        await mongoose.disconnect();
+    })()
+
 }
 
 router.get('/:animetitle', lookupAnime, (req, res) => {
+    res.status(200).send(req.results);
+})
+
+function animeList(req, res, next) {
+    (async () => {
+        await mongoose.connect("mongodb://localhost:27017/media")
+        await Anime.find({}, (err, animes) => {
+            if (err) {
+                return res.status('500').send({ errors: err });
+            }
+
+            if (animes.length == 0) return res.status(404).send({ errors: 'No Animes Found' })
+            req.results = animes;
+            next();
+        })
+        await mongoose.disconnect()
+    })()
+}
+
+router.get('/animelist', animeList, (req, res) => {
     res.status(200).send(req.results);
 })
 
