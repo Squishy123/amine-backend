d24da36264305ca03ebebac8cb5e2e1537f40a8a
Christian Wang <christian.gnaw@gmail.com>
2018-04-25 13:29:08 -0500

    Added page pool implementation

--- tests/dbs.js
+++ tests/dbs.js
@@ -2,25 +2,25 @@ const async = require('async')
 const puppeteer = require('puppeteer');
 const scrape = require('../services/scrapers/9anime.js');
 
-const threads = 5;
+const threads = 2;
 
 (async () => {
     let browser = await puppeteer.launch();
-    /* let pages = [];
+    let ps= [];
      for(let i = 0; i < threads; i++) {
-      pages.push((async() => {return await browser.newPage()})());
+      ps.push((async () => {return await browser.newPage()})());
      }
-     pages = Promise.all(pages);
-     */
-    let page = await browser.newPage();
-    let sources = await scrape.getSource(page, "https://www4.9anime.is/watch/dragon-ball-super.7jly/k4j9nr");
+     let pages = await Promise.all(ps);
+     
+    //let page = await browser.newPage();
+    let sources = await scrape.getSource(pages[0], "https://www4.9anime.is/watch/dragon-ball-super.7jly/k4j9nr");
     //console.log(sources);
 
     let puppet = async.queue(async (task, callback) => {
         //console.log("queued a task")
         await task.func.apply(null, task.args)
         callback();
-    }, 2)
+    }, threads)
 
     puppet.saturated = () => {
         console.log("Waiting for current tasks to complete...")
@@ -28,18 +28,24 @@ const threads = 5;
 
     puppet.drain = async () => {
         console.log("All tasks completed!")
+        let promises = [];
+        pages.forEach((p) => {
+            promises.push(p.close());
+        });
+        Promise.all(promises);
         await browser.close();
     }
 
-    await page.close();
+    //await page.close();
 
     async function package(url) {
-        let p = await browser.newPage();
+        let p = pages.pop();
+        //let p = await browser.newPage();
         let player = await scrape.getPlayer(p, url);
         //console.log(player);
         let video = await scrape.getVideo(p, `${player}&q=720p`);
         console.log(video);
-        await p.close();
+        pages.push(p);
     }
     
     async.each(sources, (s, c) => {
