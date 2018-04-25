a1fdf792e65bc27f56f00d5d62eb79e41df707f7
Christian Wang <christian.gnaw@gmail.com>
2018-04-25 10:42:17 -0500

    Cleaned up scrapers

--- services/scrapers/9anime.js
+++ services/scrapers/9anime.js
@@ -1,30 +1,15 @@
 const puppeteer = require('puppeteer')
 const db = require('../database.js');
 
-//schemas
-const Anime = require('../../schemas/animeSchema.js');
-const Episode = require('../../schemas/episodeSchema.js');
-const Source = require('../../schemas/sourceSchema.js');
-
 const properties = require('./properties.js');
 const whitelist = properties.whitelist;
 const blacklist = properties.blacklist;
 
 module.exports = {
     /**
-     * Divide an array into chunks of a given size
-     */
-    createChunks: function (arr, chunkSize) {
-        let results = [];
-        while (arr.length) {
-            results.push(arr.splice(0, chunkSize))
-        }
-        return results;
-    },
-    /**
     * Gets the rapidvideo player given a 9anime link
      */
-    getRapidVideoPlayer: async function (page, url) {
+    getPlayer: async function (page, url) {
         await page.evaluateOnNewDocument(() => {
             window.open = () => null;
         });
@@ -47,10 +32,10 @@ module.exports = {
     /**
     * Gets the rapidvideo player given a rapidvideo player url
      */
-    getRapidVideoFile: async function (page, url) {
+    getVideo: async function (page, url) {
         let links = [];
         await page.setRequestInterception(true);
-        let intercept = function (interceptedRequest) {
+        let intercept = (interceptedRequest) => {
             let url = interceptedRequest.url().toString();
             let wl = whitelist.some((e) => {
                 return url.includes(e);
@@ -79,9 +64,9 @@ module.exports = {
      * Grabs the links of all episodes on the given source page
      * @param {String} url 
      */
-    getSourceLinks: async function (page, url) {
+    getSource: async function (page, url) {
         try {
-            await page.goto(url, { timeout: 5000 });
+            await page.goto(url, { timeout: 5000 , waitUntil: ["domcontentloaded"]});
         } catch (err) { }
         try {
             await page.waitForSelector('#main > div > div.widget.servers > div.widget-body', { timeout: 5000 });
@@ -93,18 +78,16 @@ module.exports = {
         let sources = await page.evaluate((range) => {
             let sources = [];
             let servers = document.querySelector('#main > div > div.widget.servers > div.widget-body').children;
-            for (let p = 0; p < servers.length; p++) {
-                sources.push({ dataName: servers[p].getAttribute('data-name'), sourceList: [] });
-                let list;
-                for (let r = 0; r < range; r++) {
-                    if (range == 1) {
-                        list = document.querySelector(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p + 1}) > ul`).children;
-                    } else {
-                        list = document.querySelector(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${p + 1}) > ul:nth-child(${r + 2})`).children;
-                    }
-                    for (let l = 0; l < list.length; l++) {
-                        sources[p].sourceList.push({ href: list[l].children[0].href, index: list[l].children[0].getAttribute('data-base') });
-                    }
+            let list;
+            for (let r = 0; r < range; r++) {
+                if (range == 1) {
+                    
+                    list = document.querySelector(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${2}) > ul`).children;
+                } else {
+                    list = document.querySelector(`#main > div > div.widget.servers > div.widget-body > div:nth-child(${2}) > ul:nth-child(${2})`).children;
+                }
+                for (let l = 0; l < list.length; l++) {
+                    sources.push({ href: list[l].children[0].href, index: list[l].children[0].getAttribute('data-base') });
                 }
             }
             return sources;
@@ -116,43 +99,5 @@ module.exports = {
         let player = await this.getRapidVideoPlayer(page, url);
         let file = await this.getRapidVideoFile(page, `${player}${query}`);
         return { rapidvideo: player, url: file, quality: query }
-    },
-
-    loadChunk: async function (page, chunk, num) {
-        let data = []
-        for (let i = 0; i < chunk.length; i++) {
-            let file = [];
-            file[0] = await this.grabLink(page, chunk[i].href, '&q=360p');
-            file[1] = await this.grabLink(page, chunk[i].href, '&q=480p');
-            file[2] = await this.grabLink(page, chunk[i].href, '&q=720p');
-            file[3] = await this.grabLink(page, chunk[i].href, '&q=1080p');
-            if (file) {
-                data.push(file);
-                console.log(`${i / chunk.length * 100}% of chunk ${num} completed`)
-            } else {
-                console.log(`Error limit passed: Chunk ${num} error at : ${chunk[i]}`)
-                return;
-            }
-        }
-        console.log(`Chunk ${num} completed`)
-        return data;
-    },
-    //Adds the chunk to a database at a index
-    addChunk: async function (page, chunk, num, anime) {
-        for (let i = 0; i < chunk.length; i++) {
-            let file = [];
-            file[0] = await this.grabLink(page, chunk[i].href, '&q=360p');
-            file[1] = await this.grabLink(page, chunk[i].href, '&q=480p');
-            file[2] = await this.grabLink(page, chunk[i].href, '&q=720p');
-            file[3] = await this.grabLink(page, chunk[i].href, '&q=1080p');
-            let sources = [];
-            file.forEach((link) => {
-                sources.push({ player: link.rapidvideo, url: link.url, quality: link.quality });
-            });
-            let ep = new Episode({ id: chunk[i].index, sources: sources })
-            await ep.save((err) => {
-                console.log("Saved episode!")
-            });
-        }
     }
 }
---
+++ tests/dbs.js
@@ -0,0 +1,40 @@
+const async = require('async')
+const puppeteer = require('puppeteer');
+const scrape = require('../services/scrapers/9anime.js');
+
+const threads = 5;
+
+(async () => {
+    let browser = await puppeteer.launch();
+    /* let pages = [];
+     for(let i = 0; i < threads; i++) {
+      pages.push((async() => {return await browser.newPage()})());
+     }
+     pages = Promise.all(pages);
+     */
+    let page = await browser.newPage();
+    let sources = await scrape.getSource(page, "https://www4.9anime.is/watch/dragon-ball-super.7jly/k4j9nr");
+    //console.log(sources);
+
+    let puppet = async.queue(async (task, callback) => {
+        //console.log("queued a task")
+        await task.func.apply(null, task.args)
+        callback();
+    }, 5)
+
+    await page.close();
+
+    async function package(url) {
+        let p = await browser.newPage();
+        let player = await scrape.getPlayer(p, url);
+        //console.log(player);
+        let video = await scrape.getVideo(p, `${player}&q=720p`);
+        console.log(video);
+        await p.close();
+    }
+
+    async.each(sources, (s, c) => {
+        console.log(s);
+        puppet.push({ func: package, args: [s.href] }, () => { console.log("Completed scrape") })
+    });
+})()
