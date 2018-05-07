09c95ca1f38486fe484b52851b0a4d8b185bcdc0
Squishy123 <christian.gnaw@gmail.com>
2018-05-07 07:25:40 -0400

    Socket.io init

--- app.js
+++ app.js
@@ -1,41 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 
-//routers
-let databaseRouter = require('./routes/databaseRouter')
-app.use('/', databaseRouter)
+io.on('connection', (socket) => {
+  console.log('Client Connected!')
+  socket.on('message', (msg) => {
+    io.emit('message', msg);
+    console.log(`Message: ${msg}`)
+  })
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+  socket.on('disconnect', () => {
+    console.log('Client Disconnected!');
+  })
+})
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-
-module.exports = app;
+    
