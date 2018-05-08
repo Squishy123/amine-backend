8f8cadefe9934e677b07fa7a95df1db7a4746f14
Squishy123 <christian.gnaw@gmail.com>
2018-05-08 19:39:12 -0400

    Fixed nge speedtest

--- tests/nge.js
+++ tests/nge.js
@@ -1,9 +1,12 @@
 const main = require('../tasks/main.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-async function test() {
-    let start = new Date();
-    await main.scrape('Neon Genesis Evangelion')
-    console.log(`Execution Time: ${new Date() - start}`);
-}
-
-test();
+(async () => {
+   // let start = new Date();
+    mongoose.connect("mongodb://localhost:27017/media").then(() => {
+        console.log("Connection to database successful!")
+    }).catch(err => console.log(err))
+    await main.scrapeURL('https://www4.9anime.is/watch/neon-genesis-evangelion.179m/1ymr0m')
+//console.log(`Execution Time: ${new Date() - start}`);
+})()
